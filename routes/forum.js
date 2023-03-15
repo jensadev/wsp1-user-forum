80bd48a6571e79782ab50dd7bbfc0ec40a9bda42
Jens Andreasson <jens.andreasson@ntig.se>
2023-03-15 16:37:03 +0100

    ✨ klar, profits $

--- routes/forum.js
+++ routes/forum.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
-/* GET home page. */
-router.get('/', function (req, res) {
-    return res.json({ msg: 'Hello World' });
+const pool = require('../utils/database');
+const { authBySession } = require('../middleware/auth');
+
+const forumTable = process.env.DATABASE_FORUMTABLE;
+
+router.get('/new', authBySession, (req, res) => {
+    return res.render('forum/new.njk', {
+        title: 'New post',
+        user: req.session.uid || 0,
+    });
+});
+
+router.post('/', authBySession, async (req, res) => {
+    const { title, body } = req.body;
+    if (!title) throw new Error('Title is required');
+    if (!body) throw new Error('Body is required');
+
+    const [result] = await pool
+        .promise()
+        .query(
+            `INSERT INTO ${forumTable} (uid, title, body) VALUES (?, ?, ?)`,
+            [req.session.uid, title, body]
+        );
+
+    if (result.affectedRows === 1) {
+        return res.redirect('/');
+    }
+    throw new Error('Could not create post');
 });
 
 module.exports = router;
