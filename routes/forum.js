e13e7cf806e71f4e8580b3bcdc85060dd4072e58
Jens Andreasson <jens.andreasson@ntig.se>
2023-03-21 11:32:59 +0100

    ♻️ routes / response ✨ forum/:id

--- routes/forum.js
+++ routes/forum.js
@@ -14,20 +14,63 @@ router.get('/new', authBySession, (req, res) => {
 
 router.post('/', authBySession, async (req, res) => {
     const { title, body } = req.body;
-    if (!title) throw new Error('Title is required');
-    if (!body) throw new Error('Body is required');
+    const response = {
+        msg: '',
+        user: req.session.uid || 0,
+        errors: [],
+    };
+    // validera title och body
+    if (!title) response.errors.push('Title is required');
+    if (!body) response.errors.push('Body is required');
+    if (title.length < 3)
+        response.errors.push('Title must be at least 3 characters');
+    if (body.length < 10)
+        response.errors.push('Body must be at least 10 characters');
 
+    // sanitize title och body
+
+    if (response.errors.length === 0) {
+        const [result] = await pool
+            .promise()
+            .query(
+                `INSERT INTO ${forumTable} (uid, title, body) VALUES (?, ?, ?)`,
+                [req.session.uid, title, body]
+            );
+
+        console.log(result);
+
+        if (result.affectedRows === 1) {
+            return res.redirect(`/forum/${result.insertId}`);
+        }
+
+        response.msg = 'Could not create post';
+    }
+    return res.render('forum/new.njk', {
+        title: 'New post',
+        ...response,
+    });
+});
+
+router.get('/:id', async (req, res) => {
+    const response = {
+        msg: '',
+        user: req.session.uid || 0,
+        errors: [],
+    };
     const [result] = await pool
         .promise()
-        .query(
-            `INSERT INTO ${forumTable} (uid, title, body) VALUES (?, ?, ?)`,
-            [req.session.uid, title, body]
-        );
+        .query(`SELECT * FROM ${forumTable} WHERE id = ?`, [req.params.id]);
 
-    if (result.affectedRows === 1) {
-        return res.redirect('/');
+    if (result.length === 1) {
+        response.post = result[0];
+        return res.render('forum/post.njk', {
+            title: `${response.post.title}`,
+            ...response,
+        });
     }
-    throw new Error('Could not create post');
+
+    response.msg = 'Could not find post';
+    return res.render('forum/post.njk', response);
 });
 
 module.exports = router;
